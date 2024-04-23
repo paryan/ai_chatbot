67cb6235758e8fc5f633b90e97942fbc02a4cf42
Piyush Aryan <paryan@users.noreply.github.com>
2024-04-23 16:39:54 -0700

    Implement theme toggle and bookmark filter functionality
    UI settings are now retrieved from the SettingsCollection, and functions to toggle the dark mode theme and bookmark filtering have been created. The navigation bar was also updated with new relevant reference links and the TextDiff page. Functionality to show bookmarked threads was also introduced.

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -8,16 +8,27 @@
   import Threads from "./Pages/Threads.svelte";
   import NewThread from "./Pages/NewThread.svelte";
 
-  let darkMode = true;
+  import {SettingsCollection} from "../db/DatabaseCollection";
+  import TextDiff from "./Pages/TextDiff.svelte";
 
-  function toggleTheme(value) {
-    darkMode = value;
+  let threadHandler = Meteor.subscribe('dynamicQuery', 'SettingsCollection', {type:'UI'})
+  let darkMode = true, toggleTheme=(x) => x, toggleBookmarked = (x) => x
+
+  let showBookmarked = false
+
+  $m: {
+    let uiSettings = SettingsCollection.findOne({type:'UI'})
+    darkMode = uiSettings?.darkMode ?? false
     document.body.setAttribute('data-bs-theme', darkMode ? 'dark' : 'light');
+
+    toggleBookmarked = () => showBookmarked = !showBookmarked
+    // console.log(showBookmarked)
+    toggleTheme = () => {
+      Meteor.call('Settings: Update Dark-Mode', !darkMode)
+    }
   }
-  toggleTheme(true)
 
   let models = ['gpt-4-turbo', 'gpt-4', 'gpt-3.5-turbo']
-
 </script>
 
 <style>
@@ -39,19 +50,27 @@
 
 <nav class="navbar navbar-expand-lg sticky-top bg-body-tertiary" style="height: 3.5em !important">
   <div class="container-fluid" style="flex-direction: row">
-    <a class="navbar-brand" href="#">AI Chat</a>
+    <a class="navbar-brand" href="/">AI</a>
     <span class="navbar-toggler" style=""
           type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </span>
     <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 anavbar-nav-scroll" style="--bs-scroll-height: 100px;">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/new">New Thread</a> </li>
         <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/threads">Threads</a> </li>
-        <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/portfolio">Resume</a> </li>
-        <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/portfolio">Search</a> </li>
+<!--        <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/portfolio">Resume</a> </li>-->
+<!--        <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/portfolio">Search</a> </li>-->
+        <li class="nav-item"> <a class="nav-link" exact use:active active-class="active" href="/textDiff">Text-Diff</a> </li>
+        <li class="nav-item"> <a class="nav-link" target="_blank" rel="noreferrer" href="https://platform.openai.com/usage">Usage <SvgIcons iconName="arrow-up-right" /> </a> </li>
+        <li class="nav-item"> <a class="nav-link" target="_blank" rel="noreferrer" href="https://platform.openai.com/docs/api-reference">API Ref <SvgIcons iconName="arrow-up-right" /> </a> </li>
+        <li class="nav-item"> <a class="nav-link" target="_blank" rel="noreferrer" href="https://chat.openai.com/">Chat GPT <SvgIcons iconName="arrow-up-right" /> </a> </li>
+
       </ul>
       <form class="d-flex" role="search">
+        <a href='#' on:click|preventDefault={toggleBookmarked} class="pe-pointer" style="color:{darkMode?'white':'black'}">
+          <SvgIcons iconName="{showBookmarked ? 'bookmark-filled-lg' : 'bookmark-lg'}" />
+        </a>
         <a href='#' on:click|preventDefault={() => toggleTheme(!darkMode)} class="pe-pointer" style="color:{darkMode?'white':'black'}">
           <SvgIcons iconName="brightness-down" />
         </a>
@@ -60,11 +79,12 @@
   </div>
 </nav>
 <div class="chatContainer">
-  <Threads />
+  <Threads {showBookmarked} />
   <div class="threadDetailsContainer">
     <Route path="/" redirect="/new"/>
-    <Route path="/threads" let:meta>    {#key meta?.query?.threadId} <ThreadMessages  {models}  details={meta} /> {/key}</Route>
+    <Route path="/threads" let:meta>    {#key meta?.query?.threadId} <ThreadMessages  {models}  {showBookmarked} details={meta} /> {/key}</Route>
     <Route path="/portfolio" let:meta>  <Portfolio details={meta} /></Route>
+    <Route path="/textDiff" let:meta>   <TextDiff details={meta} {models} /></Route>
     <Route path="/new" let:meta>        <NewThread details={meta} {models} /></Route>
     <Route fallback>
       <div  style="text-align: center">
