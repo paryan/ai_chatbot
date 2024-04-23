4bc4e5fd2bcbc5683a7f01bbb085840025c9df69
Piyush Aryan <paryan@users.noreply.github.com>
2024-04-23 16:36:36 -0700

    Add TextDiff page and text-diff-logic
    A new TextDiff page added in the UI allows side-by-side comparison of input texts. Paired with it, logic for text difference and statistics such as word count, characters, sentences, paragraphs, reading level, and estimated reading time is introduced.

---
+++ imports/logic/text-diff-logic.js
@@ -0,0 +1,81 @@
+const diff = require('simple-text-diff').default
+
+const local = {
+  countSyllables(word) {
+    word = word.toLowerCase();
+    if (word.length <= 3) return 1;
+    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+    word = word.replace(/^y/, '');
+    return word.match(/[aeiouy]{1,2}/g)?.length ?? 0;
+  },
+  getReadingLevelLabel(gradeLevel) {
+    if (gradeLevel < 1) return "Early Reader";
+    if (gradeLevel < 2) return "1st Grade";
+    if (gradeLevel < 3) return "2nd Grade";
+    if (gradeLevel < 4) return "3rd Grade";
+    if (gradeLevel < 5) return "4th Grade";
+    if (gradeLevel < 6) return "5th Grade";
+    if (gradeLevel < 7) return "6th Grade";
+    if (gradeLevel < 8) return "7th Grade";
+    if (gradeLevel < 9) return "8th Grade";
+    if (gradeLevel < 10) return "9th Grade";
+    if (gradeLevel < 11) return "10th Grade";
+    if (gradeLevel < 12) return "11th Grade";
+    if (gradeLevel < 13) return "12th Grade";
+    if (gradeLevel < 14) return "College Freshman";
+    if (gradeLevel < 16) return "College Sophomore";
+    if (gradeLevel < 18) return "College Junior";
+    return "College Senior or Above";
+  },
+  calculateReadingLevel(text) {
+    const words = text.match(/\b[-?(\w+)?]+\b/gi);
+    const sentences = text.match(/[\w|\)][.?!](\s|$)/g);
+    const wordCount = words ? words.length : 0;
+    const sentenceCount = sentences ? sentences.length : 0;
+    const syllables = words ? words.reduce((acc, word) => acc + local.countSyllables(word), 0) : 0;
+    
+    if (wordCount === 0 || sentenceCount === 0) {
+      return 0; // Avoid division by zero errors
+    }
+    
+    const readingLevel = (0.39 * (wordCount / sentenceCount)) + (11.8 * (syllables / wordCount)) - 15.59;
+    return readingLevel.toFixed(2);
+  },
+  calculateReadingTime(wordsCount) {
+    const wordsPerMinute = 200;  // Average reading speed of adults
+    const minutes = wordsCount / wordsPerMinute;
+    const roundedMinutes = Math.ceil(minutes);
+    return roundedMinutes <= 1 ? "less than a minute" : `${roundedMinutes} minutes`;
+  },
+
+}
+
+module.exports = {
+  diff: (text1, text2) => {
+    const result = diff.diffPatch(text1, text2)
+    result.diffFound = result.before !== result.after
+    result.heading = result.before === result.after ? 'No Diff' : 'Diff Results'
+    result.before = result.before.replace(/<del>/g, '<del style="color:red">').replace(/<ins>/g, '<ins style="color:#06cd06">')
+    result.after = result.after.replace(/<del>/g, '<del style="color:red">').replace(/<ins>/g, '<ins style="color:#06cd06">')
+    return result
+  },
+  updateStats(text) {
+    const words = text.match(/\b[-?(\w+)?]+\b/gi);
+    const characters = text.length;
+    const sentences = text.match(/[\w|\)][.?!](\s|$)/g);
+    const paragraphs = text.match(/(\n\n|\r\r|\r\n\r\n)/g);
+    const readingLevel = local.calculateReadingLevel(text);
+    const readingLabel = characters ? local.getReadingLevelLabel(parseFloat(readingLevel)) : '-';
+    const wordCount = words ? words.length : 0;
+    const readingTime = characters ? local.calculateReadingTime(wordCount) : '-';
+    
+    return {
+      Words: words ? words.length : 0,
+      Characters: characters,
+      Sentences: sentences ? sentences.length : 0,
+      Paragraphs: paragraphs ? paragraphs.length + 1 : 0,
+      'Reading Level': readingLabel,
+      'Reading Time': readingTime
+    }
+  }
+}
---
+++ imports/ui/Pages/TextDiff.svelte
@@ -0,0 +1,148 @@
+<script>
+  let Diff = require('../../logic/text-diff-logic')
+  let _ = require('lodash')
+
+  let result = '', stats1 = Diff.updateStats(''), stats2 = Diff.updateStats(''), text1, text2
+  let tokens= {
+    1: [],
+    2: []
+  }
+
+  export let models = ['gpt-4-turbo', 'gpt-4', 'gpt-3.5-turbo']
+  let getTokens = (inputText, target) => {
+    Meteor.call('AI: Token Counter', inputText, models, (err, data) => {
+      tokens[target] = data
+    })
+  }
+
+
+</script>
+
+<style>
+
+  code {
+    display: block;
+    /*background: white;*/
+    /*color: #414141;*/
+    padding: 10px;
+    /*margin-top: 1rem;*/
+    font-size: 50%;
+  }
+
+  .heading {
+    text-align: center;
+    font-weight: bold;
+  }
+  #result {
+    padding: 0 1em;
+    font-size: 2rem;
+    /*border: 1px solid grey;*/
+  }
+  .diff {
+    border-top: 1px solid grey;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1em;
+    padding: 0.25em 0;
+  }
+  b {
+    display: block;
+    border-bottom: 1px solid black;
+  }
+  #diffForm {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1em;
+    padding: 1em;
+  }
+  .textInput {
+    display: grid;
+    grid-template-columns: auto 16em;
+    gap: 10px;
+    padding: .5em;
+    border: 1px solid grey;
+    border-radius: 5px;
+  }
+  .textInput textarea {
+    width: 100%;
+    margin: 0;
+    border: 0;
+    font-size: 1rem;
+    background: var(--bs-textarea);
+    padding: 2px 7px;
+    border-radius: 5px;
+  }
+  #stats1, #stats2 {
+    display: grid;
+    grid-template-columns: 7.5em auto;
+    gap: 0em;
+    padding: .25em;
+  }
+
+
+</style>
+
+<form id="diffForm">
+  <div class="textInput">
+    <textarea name="text1" rows="20" cols="30" class="font-monospace" placeholder="Enter first text here..." bind:value={text1} on:keyup={(event) => {
+      // console.log(event.target.value)
+      stats1 = Diff.updateStats(text1)
+      result = Diff.diff(text1, text2)
+      // console.log(result)
+    }}></textarea>
+    <div>
+      <table class="table">
+        {#each _.chain(stats1).toPairs().value() as elem}
+          <tr>
+            <th>{elem[0]}:</th>
+            <td>{elem[1]?.toLocaleString()}</td>
+          </tr>
+        {/each}
+        <tr><td colspan="2" class="text-center"><button class="btn btn-link" on:click|preventDefault={() => getTokens(text1, '1')}>Calculate Tokens</button></td></tr>
+        {#each tokens['1'] ?? [] as elem}
+          <tr>
+            <th>{elem?.model ?? ''}</th>
+            <td>{elem?.tokens?.toLocaleString() ?? ''}</td>
+          </tr>
+        {/each}
+
+      </table>
+    </div>
+  </div>
+  <div class="textInput">
+    <textarea name="text2" rows="20" cols="30" class="font-monospace" placeholder="Enter second text here..." bind:value={text2} on:keyup={(event) => {
+     // console.log(event.target.value)
+     stats2 = Diff.updateStats(text2)
+     result = Diff.diff(text1, text2)
+     // console.log(result)
+   }}></textarea>
+    <div>
+      <table class="table">
+        {#each _.chain(stats2).toPairs().value() as elem}
+          <tr>
+            <th>{elem[0]}</th>
+            <td>{elem[1]?.toLocaleString()}</td>
+          </tr>
+        {/each}
+        <tr><td colspan="2" class="text-center"><button class="btn btn-link" on:click|preventDefault={() => getTokens(text2, '2')}>Calculate Tokens</button></td></tr>
+        {#each tokens['2'] ?? [] as elem}
+          <tr>
+            <th>{elem?.model ?? ''}</th>
+            <td>{elem?.tokens?.toLocaleString() ?? ''}</td>
+          </tr>
+        {/each}
+
+      </table>
+    </div>
+  </div>
+</form>
+<div style="" id="result">
+  <div class='heading'>{result.heading ?? ''}</div>
+  {#if result.diffFound}
+    <div class="diff">
+      <code>{@html result.before}</code>
+      <code>{@html result.after}</code>
+    </div>
+  {/if}
+</div>
+
