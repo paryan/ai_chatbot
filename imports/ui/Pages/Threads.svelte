53af9763af819e7f56fc61ef52dd0dd5916eb5da
Piyush Aryan <paryan@users.noreply.github.com>
2024-04-23 17:34:01 -0700

    Add border to Threads component
    A new line of CSS has been included in Threads.svelte to add a border-right property. This border uses a variable defined elsewhere, improving component's appearance.

--- imports/ui/Pages/Threads.svelte
+++ imports/ui/Pages/Threads.svelte
@@ -41,6 +41,7 @@
     height: calc(100vh - 3.5em); /* Adjust the height as needed, here it's set to fill the viewport */
     gap: 10px; /* Optional: Adds space between grid rows and columns */
     backdrop-filter: brightness(1.5);
+    border-right: var(--bs-threads-border);
   }
 
   .newThread {
