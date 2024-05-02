395952e44c0ae6dad546a76e5b136ea413a6bc49
Piyush Aryan <paryan@users.noreply.github.com>
2024-05-01 18:05:14 -0700

    Add DropZone.svelte component to handle file uploads
    Created a new Svelte component named DropZone.svelte to handle drag and drop file uploads. It supports file filtering via an accept property, a custom drop zone UI design, and dispatches events when files are uploaded.

---
+++ imports/ui/Components/DropZone.svelte
@@ -0,0 +1,71 @@
+<script>
+  let dragging = false;
+  export let files = [], accept = ''
+  let fileInput
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  function uploadedFiles(list) {
+    // console.log(list);
+    dispatch('uploadedFiles', list)
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+    dragging = true;
+  }
+
+  function handleDragLeave(event) {
+    event.preventDefault();
+    dragging = false;
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    dragging = false;
+    files = event.dataTransfer.files;
+
+    if (accept) files = Array.from(files).filter(file => file.name.endsWith(accept) );
+
+    uploadedFiles(files)
+  }
+  function handleClick(event) {
+    event.preventDefault();
+    files = event.target.files
+    uploadedFiles(files)
+  }
+</script>
+
+<style>
+  .dropzone {
+    padding: 5px;
+    border: 2px dashed var(--dropzone-border);
+    background-color: var(--dropzone-background);
+    text-align: center;
+    transition: background-color 0.3s;
+    cursor: pointer;
+    border-radius: 5px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /*color: var(--nav-selected-text);*/
+  }
+  .dragging {
+    background-color: var(--dropzone-background-hover);
+    color: var(--nav-selected-text);
+  }
+  .fileUploader {
+    display: none;
+  }
+</style>
+
+<div class="dropzone {dragging ? 'dragging' : ''}"
+     on:dragover={handleDragOver}
+     on:dragleave={handleDragLeave}
+     on:drop={handleDrop}
+     on:click={() => fileInput.click()}
+>
+  <input type="file" class="fileUploader" accept="{accept}" bind:this={fileInput} on:change={handleClick}>
+  Drag and drop files here or click to upload.
+</div>
