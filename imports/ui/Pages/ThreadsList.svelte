6d876d134785b6469ca192050c0eb191e70b0f60
Piyush Aryan <paryan@users.noreply.github.com>
2024-05-29 11:50:48 -0700

    Refactor ThreadsList for better threads organization
    Enhanced the ThreadsList component by adding thread grouping by model and better visualizations for pinned threads and bookmarks. Made updates to the component styles for clearer thread presentation, added lodash for more efficient list processing, and updated thread fetching and handling logic for improved data handling.

--- imports/ui/Pages/ThreadsList.svelte
+++ imports/ui/Pages/ThreadsList.svelte
@@ -7,9 +7,12 @@
   import ChatTime from "../Components/ChatTime.svelte";
   export let showBookmarked
 
+  import _ from 'lodash'
+
   export let selectedThread //= document.baseURI?.replace(/.*\/threads\?threadId=(.*)/, '$1')
 
   let isLoading = true, threads = [], threadCount = 0
+  let threadGroups = [], pinnedThreads = [], hasBookmarks = []
 
   // let fromNow = (x) => M(new Date(x)).fromNow(true)
 
@@ -28,6 +31,10 @@
     isLoading = !threadHandler.ready();
     threads = ThreadsCollection.find( Q, options ).fetch();
     threads = threads.map(x => ({...x, fromNow: M(x.updatedAt, 'x').fromNow(true)?.match(/seconds/) ? 'Now' : M(x.updatedAt, 'x').fromNow(true) }))
+    pinnedThreads = {model: 'Pinned',     customGroup: true, threads: threads.filter(x => x.isPinned)}
+    // hasBookmarks  = {model: 'Bookmarks',  customGroup: true, threads: threads.filter(x => x.bookmarkedMessages)}
+    threadGroups = _.chain(threads).sortBy(['model', 'title']).groupBy('model').map((x, model) => ({model, threads: x})).reverse().value()
+    threadGroups = _.chain([pinnedThreads, hasBookmarks, ...threadGroups]).compact().filter('threads.length').value()
     threadCount = threads.length
   }
 
@@ -50,10 +57,12 @@
     "threadCount";
     height: calc(100vh - 3.5em); /* Adjust the height as needed, here it's set to fill the viewport */
     /*gap: 10px; !* Optional: Adds space between grid rows and columns *!*/
-    backdrop-filter: brightness(1.5);
+    /*backdrop-filter: brightness(1.5);*/
     border-top: var(--bs-threads-border);
     border-right: var(--bs-threads-border);
     border-top-width: 2px;
+    border-top-width: 1px;
+    background: var(--bs-threadsContainer-background);
   }
 
   .newThread {
@@ -108,47 +117,110 @@
     font-size: 80%;
   }
 
-  .selectedThread {
-    background-color: var(--bs-selected-thread);
-    font-weight: 600;
-  }
   .dropdown-toggle::after {
     display: none;
   }
+
+  .model-group {
+    /*margin: 10px;*/
+  }
+  .model-group summary {
+    margin-left: 10px;
+    margin-top: 10px;
+    margin-bottom: 5px;
+  }
+  .model-group .summaryText {
+    padding-left: 10px;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 500;
+    font-family: Roboto;
+  }
+  .model-group .threadLink {
+    padding: 3px 0 3px 35px;
+    font-family: Nunito;
+    cursor: pointer;
+    margin-bottom: 5px;
+    display: grid;
+    grid-template-columns: auto 1.25em 1.5em;
+  }
+  .model-group .threadLink:hover {
+    font-weight: 600;
+    background: var(--bs-threadsContainer-hover);
+  }
+
+  .model-group .threadLink .badge {
+    color: var(--bs-responder-content-text);
+  }
+
+  .model-group .threadLink .dropdown-toggle {
+    color: var(--dropzone-border);
+  }
+  .model-group .threadLink:hover .dropdown-toggle {
+    color: var(--bs-responder-content-text);
+  }
+
+  .model-group .threadLink.selectedThread, .model-group .threadLink.selectedThread:hover {
+    background-color: var(--bs-threadsContainer-selectedThread);
+    font-weight: 600;
+  }
+
+
 </style>
 
 <div class="threadsContainer">
   <div class="threads">
-    {#each threads as thread}
-      <div class="thread-meta {selectedThread === thread._id ? 'selectedThread' : ''}" style="cursor: pointer;">
-        <div class="thread-content text-decoration-none" on:click={() => {
-          selectedThread = thread._id
-          router.goto('/threads?threadId=' + thread._id)
-        }}>
-          <div class="thread-title text-truncate">{thread?.title ?? 'New Chat'}</div>
-          <div class="thread-ts">
-            {M(thread.createdAt, 'x').format('MM/DD/YY')} :
-            <ChatTime timestamp={thread.updatedAt} /> :
-            {thread.model?.replace(/GPT[\-]*/i, 'v').replace('-turbo', '-T')}
-            {#if thread.bookmarkedMessages}
-              : <SvgIcons iconName="bookmark-filled" /> {thread.bookmarkedMessages.toLocaleString()}
-            {/if}
-            {#if thread.onlySendLatest}<span style="position: relative; top: -2px"><SvgIcons iconName="analyze-14" /></span>{/if}
-            {#if thread.isPinned}<span style="position: relative; top: -2px"><SvgIcons iconName="pinned-filled-14" /></span>{/if}
+    {#each threadGroups as tg}
+      <details class="model-group" open>
+        <summary><span class="summaryText">{tg.model}</span></summary>
+        {#each tg.threads as thread}
+          <div class="threadLink cursor-pointer {selectedThread === thread._id ? 'selectedThread' : ''}" on:click={() => { selectedThread = thread._id;  router.goto('/threads?threadId=' + thread._id) }}>
+            <span>{thread?.title ?? 'New Chat'}</span>
+
+            <span class="badge">{#if thread.bookmarkedMessages}<SvgIcons iconName="bookmark-filled" />{:else}&nbsp;{/if}</span>
+            <div class="dropdown">
+              <span class="dropdown-toggle noAfter" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="padding:0; margin: 0; "><SvgIcons iconName="dots-vertical" /></span>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Update Thread', thread._id, {[thread.isPinned ? '$unset' : '$set']:{isPinned: true}})}>{thread.isPinned ? 'Unpin' : 'Pin To Top'}</a></li>
+                <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Duplicate Instructions', thread._id)}>Duplicate Instructions Only</a></li>
+                <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Duplicate Thread', thread._id)}>Duplicate Thread</a></li>
+                <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Update Thread', thread._id, {$set:{isArchived: true}})}>Archive</a></li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div class="dropdown">
-          <span class="dropdown-toggle noAfter" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="padding:0; margin: 0; "><SvgIcons iconName="dots-vertical" /></span>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Update Thread', thread._id, {[thread.isPinned ? '$unset' : '$set']:{isPinned: true}})}>{thread.isPinned ? 'Unpin' : 'Pin To Top'}</a></li>
-            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Duplicate Instructions', thread._id)}>Duplicate Instructions Only</a></li>
-            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Duplicate Thread', thread._id)}>Duplicate Thread</a></li>
-            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Update Thread', thread._id, {$set:{isArchived: true}})}>Archive</a></li>
-          </ul>
-        </div>
-<!--        <div class="thread-options"><SvgIcons iconName="dots-vertical" /></div>-->
-      </div>
+        {/each}
+      </details>
     {/each}
+<!--    {#each threads as thread}-->
+<!--      <div class="thread-meta {selectedThread === thread._id ? 'selectedThread' : ''}" style="cursor: pointer;">-->
+<!--        <div class="thread-content text-decoration-none" on:click={() => {-->
+<!--          selectedThread = thread._id-->
+<!--          router.goto('/threads?threadId=' + thread._id)-->
+<!--        }}>-->
+<!--          <div class="thread-title text-truncate">{thread?.title ?? 'New Chat'}</div>-->
+<!--          <div class="thread-ts">-->
+<!--            {M(thread.createdAt, 'x').format('MM/DD/YY')} :-->
+<!--            <ChatTime timestamp={thread.updatedAt} /> :-->
+<!--            {thread.model?.replace(/GPT[\-]*/i, 'v').replace('-turbo', '-T')}-->
+<!--            {#if thread.bookmarkedMessages}-->
+<!--              : <SvgIcons iconName="bookmark-filled" /> {thread.bookmarkedMessages.toLocaleString()}-->
+<!--            {/if}-->
+<!--            {#if thread.onlySendLatest}<span style="position: relative; top: -2px"><SvgIcons iconName="analyze-14" /></span>{/if}-->
+<!--            {#if thread.isPinned}<span style="position: relative; top: -2px"><SvgIcons iconName="pinned-filled-14" /></span>{/if}-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="dropdown">-->
+<!--          <span class="dropdown-toggle noAfter" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="padding:0; margin: 0; "><SvgIcons iconName="dots-vertical" /></span>-->
+<!--          <ul class="dropdown-menu">-->
+<!--            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Update Thread', thread._id, {[thread.isPinned ? '$unset' : '$set']:{isPinned: true}})}>{thread.isPinned ? 'Unpin' : 'Pin To Top'}</a></li>-->
+<!--            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Duplicate Instructions', thread._id)}>Duplicate Instructions Only</a></li>-->
+<!--            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Duplicate Thread', thread._id)}>Duplicate Thread</a></li>-->
+<!--            <li><a class="dropdown-item" href="#" on:click|preventDefault={() => Meteor.call('Threads : Update Thread', thread._id, {$set:{isArchived: true}})}>Archive</a></li>-->
+<!--          </ul>-->
+<!--        </div>-->
+<!--&lt;!&ndash;        <div class="thread-options"><SvgIcons iconName="dots-vertical" /></div>&ndash;&gt;-->
+<!--      </div>-->
+<!--    {/each}-->
   </div>
   <span class="threadCount text-muted text-center align-middle" style="font-size: .9rem;">{threadCount.toLocaleString()} Threads</span>
 </div>
